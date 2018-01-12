f594377f91275ed021123fce4c68c898b43b9e9d
Patrick Ingle <inglepatrick@yahoo.com>
2018-01-12 07:20:21 -0500

    updates

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var sys = require('sys')
 var exec = require('child_process').exec;
-
-function puts(error, stdout, stderr) {
-    sys.puts(stdout)
-}
 
 exec("npm install mysql");
